a2f1ac542e76978b2ad7e00db3a086789c0e695a
tommysun10 <tommysun10@hotmail.com>
2020-03-29 09:08:25 -0400

    Context API setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Header } from "./pages/header";
@@ -7,18 +7,22 @@ import { Login } from "./pages/login";
 import { Profile } from "./pages/profile";
 import { Task } from "./pages/task";
 import { Signup } from "./pages/signup";
+import { UserProvider } from "./services/authContext";
 
 const App = () => {
+  const [fireBaseId, setFireBaseId] = useState("");
   return (
     <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact={true} path="/" component={Home} />
-        <Route path="/task" component={Task} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
+      <UserProvider value={{ fireBaseId, setFireBaseId }}>
+        <Header />
+        <Switch>
+          <Route exact={true} path="/" component={Home} />
+          <Route path="/task" component={Task} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </UserProvider>
     </BrowserRouter>
   );
 };
