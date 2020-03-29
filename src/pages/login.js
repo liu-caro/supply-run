37986cd05d852c8d8c58672a3b0adb63ca5fd83d
Tommy Sun <tommysun10@hotmail.com>
2020-03-28 22:55:44 -0400

    not working login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,72 @@
 import React from "react";
-import TextField from '@material-ui/core/TextField';
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Please enter a valid email")
+    .required("Please enter a valid email"),
+  password: Yup.string().required("Please enter a password")
+});
 
 const Login = () => {
-  return <>
-    <TextField id="standard-basic" label="Email" />
-    <TextField id="standard-basic" label="Password" />
-  </>;
+  return (
+    <>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={LoginSchema}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field
+              component={TextField}
+              name="email"
+              type="email"
+              label="Email"
+            />
+            <Field
+              component={TextField}
+              name="password"
+              type="password"
+              label="Password"
+            />
+            <Button type="submit">Submit</Button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
 };
 
 export { Login };
+
+{
+  /* <div>
+<h1>Any place in your app!</h1>
+<Formik
+  initialValues={{ email: "", password: "" }}
+  validationSchema={LoginSchema}
+  onSubmit={values => {
+    console.log(values);
+  }}
+>
+  {({ isSubmitting }) => (
+    <Form>
+      <Field type="email" name="email" />
+      <ErrorMessage name="email" component="div" />
+      <Field type="password" name="password" />
+      <ErrorMessage name="password" component="div" />
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </Form>
+  )}
+</Formik>
+</div> */
+}
